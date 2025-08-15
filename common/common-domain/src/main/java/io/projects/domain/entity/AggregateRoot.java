2fca9f733d5072313173d2bc3ab934c8d5cc5094
Dinesh <dinesh@Dineshs-MacBook-Air.local>
2025-08-15 13:58:24 +0530

    Inital setup for creating domain model of order service

---
+++ common/common-domain/src/main/java/io/projects/domain/entity/AggregateRoot.java
@@ -0,0 +1,4 @@
+package io.projects.domain.entity;
+
+public abstract class AggregateRoot<ID> extends BaseEntity<ID> {
+}
---
+++ common/common-domain/src/main/java/io/projects/domain/valueobject/Money.java
@@ -0,0 +1,38 @@
+package io.projects.domain.valueobject;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Money {
+    private final BigDecimal amount;
+
+    public Money(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public boolean isGreaterThanZero(){
+        return this.amount !=null && this.amount.compareTo(BigDecimal.ZERO) > 0;
+    }
+
+    public boolean isGreaterThan(Money money){
+        return this.amount != null && this.amount.compareTo(money.getAmount()) > 0;
+    }
+
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Money money = (Money) o;
+        return Objects.equals(amount, money.amount);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(amount);
+    }
+}
